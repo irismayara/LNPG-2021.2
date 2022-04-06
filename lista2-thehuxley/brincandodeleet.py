bbe33fe546da9aa5013bd838e0ddbac80c411c08
Iris Mayara <mayaraim.114@gmail.com>
2022-04-06 00:28:14 -0300

    lista 2 do the huxley em andamento

--- lista2-thehuxley/brincandodeleet.py
+++ lista2-thehuxley/brincandodeleet.py
@@ -1,3 +1,18 @@
+# Leet é uma maneira de escrever palavras na qual algumas letras, sílabas ou palavras são substituídas por números ou símbolos. Isso é muito usado no contexto da informática para confundir leitores "não iniciados” ou escrever de forma resumida (por exemplo, na escrita de tweets). Dentro desse contexto, você vai escrever um programa que que transforma palavras na grafia comum em palavras na escrita Leet de forma invertida. Ao final, o programa deve informar a quantidade de substituições realizadas.
+
+# Use a seguinte codificação:
+# a, A: @
+# e, E: 3
+# i, I: 1
+# o, O: 0
+# t, T: 7
+# s, S: 5
+
+# Não haverão palavras acentuadas.
+# Se o usuário informar uma string vazia, o programa emitirá uma mensagem de erro e a quantidade 0 (zero).
+# Se o usuário informar uma string que contém pelo menos um número, o programa escreverá uma mensagem de erro e a quantidade 0 (zero).
+# O código deve fazer uso de funções sempre que necessário, observando os princípios da modularização e separação de objetivos.
+
 def codificar(string, codigos):
     for i in range(len(string)):
         for codigo in codigos:
--- lista2-thehuxley/faixaetaria.py
+++ lista2-thehuxley/faixaetaria.py
@@ -1,3 +1,17 @@
+# Uma determinada professora quer treinar as pessoas para que elas saibam em que faixa etária elas estão. Para isso ela lhe pede que faça um programa que leia a idade das pessoas e indiquem qual sua faixa etária. O programa deve parar quando é feita a leitura da idade 0. 
+
+# Para as demais idades a saída deve ser: 
+# Idade: de 01 a 11 anos - Saída: "Você é uma criança" 
+# Idade: de 12 a 17 anos - Saída: "Você é um adolescente" 
+# Idade: de 18 a 35 anos - Saída: "Você é um jovem"
+# Idade: de 36 a 64 anos - Saída: "Você é um adulto"
+# Idade: acima de 65 anos - Saída: "Você é um idoso"
+
+#obs: Quando for digitado uma idade negativa, é preciso informar que a pessoa ainda não nasceu.  
+
+# ENTRADA: Idades aleatórias, termina a digitação com 0.
+# SAIDA: Faixa etária de acordo com a idade da entrada.
+
 while True:
     idade = int(input())
     if(idade == 0):
